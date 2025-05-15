f36628b134a33399f38f66aac9cd41ef3c30d5bd
kievzenit <kir.backender@gmail.com>
2025-05-15 16:14:58 +0300

    skeleton for subscriptions repository created

---
+++ internal/database/repositories/subscriptions_repository.go
@@ -0,0 +1,54 @@
+package repositories
+
+import (
+	"context"
+
+	"github.com/google/uuid"
+	"github.com/kievzenit/genesis-case/internal/database"
+	"github.com/kievzenit/genesis-case/internal/models"
+)
+
+type SubscriptionRepository interface {
+	IsUserSubscribedContext(ctx context.Context, email string, city string) (bool, error)
+	SubscribeContext(
+		ctx context.Context,
+		email string,
+		token uuid.UUID,
+		city string,
+		frequency models.Frequency,
+	) error
+	ConfirmSubscriptionContext(ctx context.Context, token uuid.UUID) error
+	UnsubscribeContext(ctx context.Context, token uuid.UUID) error
+}
+
+func NewSubscriptionRepository(db database.Database) SubscriptionRepository {
+	return &subscriptionRepository{
+		db: db,
+	}
+}
+
+type subscriptionRepository struct {
+	db database.Database
+}
+
+func (r *subscriptionRepository) ConfirmSubscriptionContext(ctx context.Context, token uuid.UUID) error {
+	panic("unimplemented")
+}
+
+func (r *subscriptionRepository) IsUserSubscribedContext(ctx context.Context, email string, city string) (bool, error) {
+	panic("unimplemented")
+}
+
+func (r *subscriptionRepository) SubscribeContext(
+	ctx context.Context,
+	email string,
+	token uuid.UUID,
+	city string,
+	frequency models.Frequency,
+) error {
+	panic("unimplemented")
+}
+
+func (r *subscriptionRepository) UnsubscribeContext(ctx context.Context, token uuid.UUID) error {
+	panic("unimplemented")
+}
