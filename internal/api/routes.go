3474f3c359a4c6e6607fb66723b36ded732118b1
kievzenit <kir.backender@gmail.com>
2025-05-15 16:20:11 +0300

    registered subscribe endpoint in routes

--- internal/api/routes.go
+++ internal/api/routes.go
@@ -3,13 +3,26 @@ package routes
 import (
 	"github.com/gin-gonic/gin"
 	"github.com/kievzenit/genesis-case/internal/api/handlers"
+	"github.com/kievzenit/genesis-case/internal/database"
 	"github.com/kievzenit/genesis-case/internal/services"
 )
 
-func RegisterRoutes(weatherService services.WeatherService) *gin.Engine {
+func RegisterRoutes(
+	weatherService services.WeatherService,
+	emailService services.EmailService,
+	database database.Database,
+	txManager *database.TransactionManger,
+) *gin.Engine {
 	r := gin.Default()
 
 	r.GET("weather", handlers.GetWeatherForCityHandler(weatherService))
+
+	r.POST("subscribe", handlers.SubscribeForWeatherHandler(
+		weatherService,
+		emailService,
+		database,
+		txManager,
+	))
 
 	return r
 }
