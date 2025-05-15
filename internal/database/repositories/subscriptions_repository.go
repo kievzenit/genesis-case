eb318004a69febb6b352d12422c49eb0eca41c05
kievzenit <kir.backender@gmail.com>
2025-05-15 16:51:22 +0300

    added get subscription by frequency method to subscriptions repository

--- internal/database/repositories/subscriptions_repository.go
+++ internal/database/repositories/subscriptions_repository.go
@@ -20,16 +20,25 @@ type SubscriptionRepository interface {
 	) error
 	ConfirmSubscriptionContext(ctx context.Context, token uuid.UUID) error
 	UnsubscribeContext(ctx context.Context, token uuid.UUID) error
+	GetSubscriptionsByFrequencyContext(
+		ctx context.Context,
+		frequency models.Frequency,
+	) ([]models.Subscription, error)
 }
 
 func NewSubscriptionRepository(db database.Database) SubscriptionRepository {
-	return &subscriptionRepository{
-		db: db,
-	}
+	return &subscriptionRepository{db}
 }
 
 type subscriptionRepository struct {
 	db database.Database
+}
+
+func (r *subscriptionRepository) GetSubscriptionsByFrequencyContext(
+	ctx context.Context,
+	frequency models.Frequency,
+) ([]models.Subscription, error) {
+	panic("unimplemented")
 }
 
 var ErrConfirmationTokenNotFound = errors.New("confirmation token not found")
@@ -39,7 +48,19 @@ func (r *subscriptionRepository) ConfirmSubscriptionContext(ctx context.Context,
 }
 
 func (r *subscriptionRepository) IsUserSubscribedContext(ctx context.Context, email string, city string) (bool, error) {
-	panic("unimplemented")
+	var exists bool
+
+	err := r.db.QueryRowContext(
+		ctx,
+		"SELECT EXISTS(SELECT 1 FROM user_subscriptions WHERE email = $1 AND city = $2 LIMIT 1)",
+		email,
+		city,
+	).Scan(&exists)
+	if err != nil {
+		return false, err
+	}
+
+	return exists, nil
 }
 
 func (r *subscriptionRepository) SubscribeContext(
