c93069d24202833e9136a12de99ebf0820c5e9b5
kievzenit <kir.backender@gmail.com>
2025-05-15 16:10:59 +0300

    email service skeleton created

---
+++ internal/services/email_service.go
@@ -0,0 +1,37 @@
+package services
+
+import (
+	"net/smtp"
+
+	"github.com/google/uuid"
+)
+
+type WeatherData struct {
+	City        string
+	Temp        float64
+	Humidity    float64
+	Description string
+}
+
+type EmailService interface {
+	SendConfirmationEmail(email string, token uuid.UUID) error
+	SendWeatherReport(email string, city string, weatherData WeatherData) error
+}
+
+func NewEmailService() EmailService {
+	return &emailService{
+		smtpClient: nil,
+	}
+}
+
+type emailService struct {
+	smtpClient *smtp.Client
+}
+
+func (e *emailService) SendConfirmationEmail(email string, token uuid.UUID) error {
+	panic("unimplemented")
+}
+
+func (e *emailService) SendWeatherReport(email string, city string, weatherData WeatherData) error {
+	panic("unimplemented")
+}
