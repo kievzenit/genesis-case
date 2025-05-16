f74990844e82afee29a1bfcfd68a3f2deb95d44d
kievzenit <kir.backender@gmail.com>
2025-05-16 20:17:59 +0300

    added implementation to confirmation emails repository

--- internal/database/repositories/confirmation_emails_repository.go
+++ internal/database/repositories/confirmation_emails_repository.go
@@ -2,6 +2,7 @@ package repositories
 
 import (
 	"context"
+	"time"
 
 	"github.com/kievzenit/genesis-case/internal/database"
 	"github.com/kievzenit/genesis-case/internal/models"
@@ -25,18 +26,66 @@ func (r *confirmationEmailsRepository) UpdateConfirmationEmail(
 	ctx context.Context,
 	confirmationEmail models.ConfirmationEmail,
 ) error {
-	panic("unimplemented")
+	_, err := r.db.ExecContext(
+		ctx,
+		`UPDATE pending_confirmation_emails
+		SET completed = $1, attempts = $2, next_try_after = $3
+		WHERE id = $4`,
+		confirmationEmail.Completed,
+		confirmationEmail.Attempts,
+		confirmationEmail.NextTryAfter,
+		confirmationEmail.Id,
+	)
+	return err
 }
 
 func (r *confirmationEmailsRepository) GetConfirmationEmailsToSend(
 	ctx context.Context,
 ) ([]models.ConfirmationEmail, error) {
-	panic("unimplemented")
+	nowUtc := time.Now().UTC()
+	confirmationEmailsRows, err := r.db.QueryContext(
+		ctx,
+		`SELECT id, to_address, token, attempts, next_try_after
+		FROM pending_confirmation_emails
+		WHERE completed = false AND next_try_after <= $1 AND attempts < 3
+		FOR UPDATE SKIP LOCKED`,
+		nowUtc,
+	)
+	if err != nil {
+		return nil, err
+	}
+	defer confirmationEmailsRows.Close()
+
+	var confirmationEmails []models.ConfirmationEmail
+	for confirmationEmailsRows.Next() {
+		var confirmationEmail models.ConfirmationEmail
+		err := confirmationEmailsRows.Scan(
+			&confirmationEmail.Id,
+			&confirmationEmail.ToAddress,
+			&confirmationEmail.Token,
+			&confirmationEmail.Attempts,
+			&confirmationEmail.NextTryAfter,
+		)
+		if err != nil {
+			return nil, err
+		}
+		confirmationEmails = append(confirmationEmails, confirmationEmail)
+	}
+
+	return confirmationEmails, nil
 }
 
 func (r *confirmationEmailsRepository) StoreConfirmationEmail(
 	ctx context.Context,
 	confirmationEmail models.ConfirmationEmail,
 ) error {
-	panic("unimplemented")
+	_, err := r.db.ExecContext(
+		ctx,
+		`INSERT INTO pending_confirmation_emails (to_address, token, next_try_after)
+		VALUES ($1, $2, $3)`,
+		confirmationEmail.ToAddress,
+		confirmationEmail.Token,
+		confirmationEmail.NextTryAfter,
+	)
+	return err
 }
