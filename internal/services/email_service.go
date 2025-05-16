167f8078b162327db51fbfe5f1588941ad3bbb9f
kievzenit <kir.backender@gmail.com>
2025-05-16 13:24:36 +0300

    updated email service, now it accepts baseURL, changed signature of methods, implemented methods to send actual emails

--- internal/services/email_service.go
+++ internal/services/email_service.go
@@ -1,10 +1,16 @@
 package services
 
 import (
-	"net/smtp"
+	"bytes"
+	"fmt"
+	"html/template"
+	"time"
 
 	"github.com/google/uuid"
 	"github.com/kievzenit/genesis-case/internal/config"
+	"github.com/kievzenit/genesis-case/internal/models"
+	"github.com/kievzenit/genesis-case/internal/utils"
+	"gopkg.in/gomail.v2"
 )
 
 type WeatherData struct {
@@ -15,24 +21,136 @@ type WeatherData struct {
 }
 
 type EmailService interface {
-	SendConfirmationEmail(email string, token uuid.UUID) error
-	SendWeatherReport(email string, city string, weatherData WeatherData) error
+	SendConfirmationEmail(email string, city string, frequency models.Frequency, token uuid.UUID) error
+	SendWeatherReport(email string, city string, frequency models.Frequency, weatherData WeatherData) error
 }
 
-func NewEmailService(cfg *config.EmailServiceConfig) EmailService {
+func NewEmailService(baseURL string, cfg *config.EmailServiceConfig) EmailService {
 	return &emailService{
-		smtpClient: nil,
+		from:    cfg.From,
+		baseURL: baseURL,
+		dialer: gomail.NewDialer(
+			cfg.Host,
+			cfg.Port,
+			cfg.Username,
+			cfg.Password,
+		),
 	}
 }
 
 type emailService struct {
-	smtpClient *smtp.Client
+	from    string
+	baseURL string
+	dialer  *gomail.Dialer
 }
 
-func (e *emailService) SendConfirmationEmail(email string, token uuid.UUID) error {
-	panic("unimplemented")
+func convertFrequencyToReportPeriod(frequency models.Frequency) string {
+	switch frequency {
+	case models.Daily:
+		return "24 hours"
+	case models.Hourly:
+		return "1 hour"
+	default:
+		panic("unknown frequency")
+	}
 }
 
-func (e *emailService) SendWeatherReport(email string, city string, weatherData WeatherData) error {
-	panic("unimplemented")
+func (e *emailService) SendConfirmationEmail(
+	email string,
+	city string,
+	frequency models.Frequency,
+	token uuid.UUID,
+) error {
+	msg := gomail.NewMessage()
+
+	msg.SetHeader("From", e.from)
+	msg.SetHeader("To", email)
+	msg.SetHeader("Subject", "Weather subscription confirmation")
+
+	emailBodyTemplate, err := template.ParseFiles("templates/email/subscription_confirmation_email.html")
+	if err != nil {
+		return err
+	}
+
+	emailBodyTemplate = emailBodyTemplate.Funcs(template.FuncMap{
+		"upperFirstLetter": utils.UpperFirstLetter,
+	})
+
+	var emailBodyBuf bytes.Buffer
+	err = emailBodyTemplate.Execute(&emailBodyBuf, struct {
+		CustomerEmail    string
+		City             string
+		Frequency        string
+		Date             string
+		ReportPeriod     string
+		ConfirmationLink string
+	}{
+		CustomerEmail:    email,
+		City:             city,
+		Frequency:        string(frequency),
+		Date:             time.Now().Format("January 2, 2006"),
+		ReportPeriod:     convertFrequencyToReportPeriod(frequency),
+		ConfirmationLink: fmt.Sprintf("http://%s/confirm/%s", e.baseURL, token.String()),
+	})
+	if err != nil {
+		return err
+	}
+
+	msg.SetBody("text/html", emailBodyBuf.String())
+
+	return e.dialer.DialAndSend(msg)
+}
+
+func (e *emailService) SendWeatherReport(
+	email string,
+	city string,
+	frequency models.Frequency,
+	weatherData WeatherData,
+) error {
+	msg := gomail.NewMessage()
+
+	msg.SetHeader("From", e.from)
+	msg.SetHeader("To", email)
+	msg.SetHeader("Subject", fmt.Sprintf("Weather report for %s", city))
+
+	emailBodyTemplate, err := template.ParseFiles("templates/email/weather_report_email.html")
+	if err != nil {
+		return err
+	}
+
+	emailBodyTemplate = emailBodyTemplate.Funcs(template.FuncMap{
+		"upperFirstLetter": utils.UpperFirstLetter,
+	})
+
+	var emailBodyBuf bytes.Buffer
+	err = emailBodyTemplate.Execute(&emailBodyBuf, struct {
+		Frequency       string
+		Date            string
+		City            string
+		FullDate        string
+		Time            string
+		Description     string
+		Temperature     string
+		Humidity        string
+		UnsubscribeLink string
+		CustomerEmail   string
+	}{
+		Frequency:       string(frequency),
+		Date:            time.Now().Format("January 2, 2006"),
+		City:            weatherData.City,
+		FullDate:        time.Now().Format("Monday, January 2, 2006"),
+		Time:            time.Now().Format("15:04"),
+		Description:     weatherData.Description,
+		Temperature:     fmt.Sprintf("%.2f", weatherData.Temp),
+		Humidity:        fmt.Sprintf("%.2f", weatherData.Humidity),
+		UnsubscribeLink: fmt.Sprintf("http://%s/unsubscribe/%s", e.baseURL, email),
+		CustomerEmail:   email,
+	})
+	if err != nil {
+		return err
+	}
+
+	msg.SetBody("text/html", emailBodyBuf.String())
+
+	return e.dialer.DialAndSend(msg)
 }
